319e196605620535cb0d5d66fd71e0dc14e139a1
Rabby <rabby410@gmail.com>
2023-01-25 17:46:05 +0600

    aaa

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import Head from 'next/head'
-import { Inter } from '@next/font/google'
 import Homepage from './allPages/Index'
 
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
